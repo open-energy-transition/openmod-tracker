f39e1cb1f1b4e649a6a5c5527f157ca6bc5b56e1
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2025-09-23 09:21:13 +0100

    Minor text rearrangement

--- website/pages/1_👤_Deep_Dive_-_User_Interaction_Analysis.py
+++ website/pages/1_👤_Deep_Dive_-_User_Interaction_Analysis.py
@@ -141,6 +141,12 @@ def preamble():
     """Text to show before the user data plots."""
     st.markdown(
         """
+        Interactions generally come in the form of
+        [stars](https://docs.github.com/en/get-started/exploring-projects-on-github/saving-repositories-with-stars),
+        [forks](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/about-forks),
+        [watches](https://dl.acm.org/doi/10.1145/2597073.2597114),
+        [issues](https://docs.github.com/en/issues/tracking-your-work-with-issues/about-issues), and
+        [contributions](https://docs.github.com/en/repositories/viewing-activity-and-data-for-your-repository/viewing-a-projects-contributors#about-contributors).
         We have then gathered data on the GitHub users linked to those interactions to find their origin country and attempted to classify them as being from one of 5 main groups:
 
         🎓 **academic** - an academic institution (e.g., university).
@@ -228,15 +234,9 @@ if __name__ == "__main__":
 
     st.title("Tool User Interaction Analysis")
     st.text(
-        "A wide variety of users interact with the hosted repositories of each of our tracked energy modelling tools."
+        "A wide variety of users interact with the hosted repositories of each of our tracked energy modelling tools. "
+        "On this page, you can explore these interactions for all GitHub-hosted tools."
     )
-    st.markdown(
-        "These interactions generally come in the form of [stars](https://docs.github.com/en/get-started/exploring-projects-on-github/saving-repositories-with-stars), [forks](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/about-forks), [watches](https://dl.acm.org/doi/10.1145/2597073.2597114), [issues](https://docs.github.com/en/issues/tracking-your-work-with-issues/about-issues), and [contributions](https://docs.github.com/en/repositories/viewing-activity-and-data-for-your-repository/viewing-a-projects-contributors#about-contributors)."
-    )
-    st.text(
-        "On this page, we have collated all these interactions for all GitHub-hosted tools."
-    )
-
     user_stats_dir = Path(__file__).parent.parent.parent / "user_analysis" / "output"
     df_vis = create_vis_table(user_stats_dir)
 
